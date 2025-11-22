f9d6f51d53abefdd4a2277fe9ec5023a7beced1e
lkdjfalnnlvz <155760188+lkdjfalnnlvz@users.noreply.github.com>
2025-11-22 13:19:01 +0900

    Refactor WebM_Premiere_Import.h for libav integration
    Removed old copyright notice and added new includes for libav libraries.

--- src/premiere/WebM_Premiere_Import.h
+++ src/premiere/WebM_Premiere_Import.h
@@ -1,106 +1,47 @@
-///////////////////////////////////////////////////////////////////////////
-//
-// Copyright (c) 2013, Brendan Bolles
-// 
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-// *	   Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-// *	   Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-///////////////////////////////////////////////////////////////////////////
+#ifndef _WEBM_PREMIERE_IMPORT_H_
+#define _WEBM_PREMIERE_IMPORT_H_
 
-// ------------------------------------------------------------------------
-//
-// WebM plug-in for Premiere
-//
-// by Brendan Bolles <brendan@fnordware.com>
-//
-// ------------------------------------------------------------------------
+#include "PrSDKImport.h"
+#include "PrSDKTypes.h"
+#include "PrSDKPPixCreatorSuite.h"
+#include "PrSDKPPixCacheSuite.h"
+#include "PrSDKPPixSuite.h"
+#include "PrSDKTimeSuite.h"
+#include "PrSDKStreamSuite.h"
 
-
-
-
-#ifndef WEBM_PREMIERE_IMPORT_H
-#define WEBM_PREMIERE_IMPORT_H
-
-#include	"PrSDKStructs.h"
-#include	"PrSDKImport.h"
-#include	"PrSDKExport.h"
-#include	"PrSDKExportFileSuite.h"
-#include	"PrSDKExportInfoSuite.h"
-#include	"PrSDKExportParamSuite.h"
-#include	"PrSDKExportProgressSuite.h"
-#include	"PrSDKErrorSuite.h"
-#include	"PrSDKMALErrors.h"
-#include	"PrSDKMarkerSuite.h"
-#include	"PrSDKSequenceRenderSuite.h"
-#include	"PrSDKSequenceAudioSuite.h"
-#include	"PrSDKClipRenderSuite.h"
-#include	"PrSDKPPix2Suite.h"
-#include	"PrSDKPPixCreatorSuite.h"
-#include	"PrSDKPPixCacheSuite.h"
-#include	"PrSDKImporterFileManagerSuite.h"
-#include	"PrSDKMemoryManagerSuite.h"
-#include	"PrSDKWindowSuite.h"
-#include	"PrSDKAppInfoSuite.h"
-#include	"SDK_Segment_Utils.h"
-
-
-
-#if IMPORTMOD_VERSION <= IMPORTMOD_VERSION_9
-typedef long PrivateDataPtr;
-
-typedef int csSDK_int32;
-typedef int csSDK_size_t;
-typedef long int RowbyteType;
-
-#define CAST_REFNUM(REFNUM)		(REFNUM)
-#define CAST_FILEREF(FILEREF)	(FILEREF)
-
-#ifdef PRMAC_ENV
-typedef SInt16 FSIORefNum;
-#endif
-
-#else
-typedef void * PrivateDataPtr;
-typedef csSDK_int32 RowbyteType;
-
-#ifdef PRMAC_ENV
-#define CAST_REFNUM(REFNUM)		reinterpret_cast<intptr_t>(REFNUM)
-#define CAST_FILEREF(FILEREF)	reinterpret_cast<imFileRef>(FILEREF)
-#else
-#define CAST_REFNUM(REFNUM)		(REFNUM)
-#define CAST_FILEREF(FILEREF)	(FILEREF)
-#endif
-
-#endif
-
-// Declare plug-in entry point with C linkage
 extern "C" {
-PREMPLUGENTRY DllExport xImportEntry (csSDK_int32	selector, 
-									  imStdParms	*stdParms, 
-									  void			*param1, 
-									  void			*param2);
-
+    #include <libavformat/avformat.h>
+    #include <libavcodec/avcodec.h>
+    #include <libswscale/swscale.h>
+    #include <libavutil/imgutils.h>
+    #include <libavutil/opt.h>
 }
 
-#endif //WEBM_PREMIERE_IMPORT_H
+typedef struct
+{
+    csSDK_int32             importerID;
+    
+    AVFormatContext*        fmt_ctx;       
+    
+    int                     videoStreamIdx;
+    AVCodecContext*         videoCodecCtx;  
+    AVStream*               videoStream;  
+    SwsContext*             swsCtx;        
+    
+    int                     audioStreamIdx;
+    AVCodecContext*         audioCodecCtx;
+    AVStream*               audioStream;
+
+    csSDK_int32             width;
+    csSDK_int32             height;
+    int64_t                 duration;
+    PlugMemoryFuncsPtr      memFuncs;
+    SPBasicSuite            *BasicSuite;
+    PrSDKPPixCreatorSuite   *PPixCreatorSuite;
+    PrSDKPPixCacheSuite     *PPixCacheSuite;
+    PrSDKPPixSuite          *PPixSuite;
+    PrSDKTimeSuite          *TimeSuite;
+
+} ImporterLocalRec8, *ImporterLocalRec8Ptr, **ImporterLocalRec8H;
+
+#endif
